385821efb527373c0097b3bc8a217a28471d16dc
Ben Simms <ben@bensimms.moe>
2025-09-27 16:52:10 +0200

    License and readme

---
+++ README.el
@@ -0,0 +1,5 @@
+# Nucleo fuzzy search provider for emacs
+
+This library provides a fuzzy search backend using [nucleo](https://github.com/helix-editor/nucleo).
+
+As nucleo is a rust library, this is therefore implemented as an emacs rust module.
